f5c73afdfdb70a3b68f0b863bc80c5170db63fc5
Manroj Gill <manrojgill@hotmail.com>
2026-04-10 19:45:46 -0600

    updateCORS

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ connectDB()
 
 
 app.use(express.json())
-app.use(cors( {origin: process.env.FRONTEND_URL, credentials: true}))
+app.use(cors( {origin: process.env.FRONTEND_URI, credentials: true}))
 
 app.use('/api/user',userRouter)
 app.use('/api/score',authUser, scoreRouter)
